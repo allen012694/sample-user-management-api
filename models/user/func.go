fa4ad3103fa10985e7eb1e2a6f29761d9d4bc3d0
Allen Nguyen <allen012694@gmail.com>
2021-08-10 04:35:31 +0700

    Include profile pictures of a user

--- models/user/func.go
+++ models/user/func.go
@@ -1,19 +1,24 @@
 package user
 
 import (
+	"github.com/allen012694/usersystem/models/profile_picture"
 	"github.com/allen012694/usersystem/types"
 	"gorm.io/gorm"
 )
 
 func GetUserByUsername(tx *gorm.DB, username string) (*User, error) {
 	var user *User
-	err := tx.Where(&User{Status: 1, Username: username}).First(&user).Error
+	err := tx.Model(&User{}).Preload("ProfilePictures", func(q *gorm.DB) *gorm.DB {
+		return q.Where(&profile_picture.ProfilePicture{Status: 1}).Order("updated_at DESC").Order("id DESC")
+	}).Where(&User{Status: 1, Username: username}).First(&user).Error
 	return user, err
 }
 
 func GetUserById(tx *gorm.DB, id int64) (*User, error) {
 	var user *User
-	err := tx.Where(&User{Status: 1}).First(&user, id).Error
+	err := tx.Model(&User{}).Preload("ProfilePictures", func(q *gorm.DB) *gorm.DB {
+		return q.Where(&profile_picture.ProfilePicture{Status: 1}).Order("updated_at DESC").Order("id DESC")
+	}).Where(&User{Status: 1}).First(&user, id).Error
 	return user, err
 }
 
